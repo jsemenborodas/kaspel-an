5a873cef556ab5ea4a8e81c57ff943a92d05c002
jsemenborodas <jsemenborodas@gmail.com>
2025-06-30 17:22:03 +0300

    fix: fix problem with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   base: '/kaspel-an/',
-  plugins: [react()],
+  plugins: [react(),],
 })
